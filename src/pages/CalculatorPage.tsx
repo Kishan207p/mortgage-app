4e1c9f591cb3c8d97574a4bdd5f66e76a88302eb
Kishan Patel <kishan207.p@gmail.com>
2024-03-27 15:23:11 -0400

    sub1
    .

--- src/pages/CalculatorPage.tsx
+++ src/pages/CalculatorPage.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Calculator from "../components/Calculator";
-import MortgageCalculator from "../components/MortgageCalc";
+import MortgageCalculator from "../components/MortgageCalculator";
 
-const CalculatorPage  = () => {
-    return (
-      <div className="calculator-page">
-        <Calculator/>
-        <MortgageCalculator/>
-      </div>
-    );
+const CalculatorPage = () => {
+  return (
+    <div className="calculator-page">
+      <Calculator />
+      <MortgageCalculator />
+    </div>
+  );
 };
 
 export default CalculatorPage;
