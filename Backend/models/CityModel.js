3a04d6f4a4fd0a9f4026b015fdb2c8a69f5d5834
ronak-patel-03 <61611211+ronak-patel-03@users.noreply.github.com>
2024-03-27 23:36:38 -0400

    Update CityModel.js

--- Backend/models/CityModel.js
+++ Backend/models/CityModel.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
 const citySchema = new mongoose.Schema({
-  city_id: Number,
-  description: String,
-  province_id: Number,
+  cities: [{
+    city_id: Number,
+    description: String
+  }],
+  province_id: Number
 });
 
 module.exports = mongoose.model("City", citySchema);
