370d7f3b64ccb7c13d65559eabe7192111fc0cdc
Kishan Patel <kishan207.p@gmail.com>
2024-03-29 22:12:19 -0400

    Update MortgageForm.tsx

--- Frontend/src/components/MortgageForm.tsx
+++ Frontend/src/components/MortgageForm.tsx
@@ -91,48 +91,48 @@ const MortgageForm: React.FC<MortgageFormProps> = ({
   return (
     <div className="font-medium font-sans">
       <div className="w-full">
-      <label className="font-medium font-sans">Principal:</label>
-      <input
-        type="number"
-        value={principal}
-        onChange={(e) => {
-          if (e.target instanceof HTMLInputElement) {
-            onPrincipalChange(Number(e.target.value));
-          }
-        }}
-        disabled={inputsDisabled}
-        className="w-full mb-2 border border-gray-500 rounded-md"
-      />
+        <label className="font-medium font-sans">Principal:</label>
+        <input
+          type="number"
+          value={principal}
+          onChange={(e) => {
+            if (e.target instanceof HTMLInputElement) {
+              onPrincipalChange(Number(e.target.value));
+            }
+          }}
+          disabled={inputsDisabled}
+          className="w-full mb-2 border border-gray-500 rounded-md"
+        />
       </div>
 
       <div className="w-full">
-      <label>Interest Rate:</label>
-      <input
-        type="number"
-        value={interestRate}
-        onChange={(e) => {
-          if (e.target instanceof HTMLInputElement) {
-            onInterestRateChange(Number(e.target.value));
-          }
-        }}
-        disabled={inputsDisabled}
-        className="w-full mb-2 border border-gray-500 rounded-md"
-      />
+        <label>Interest Rate:</label>
+        <input
+          type="number"
+          value={interestRate}
+          onChange={(e) => {
+            if (e.target instanceof HTMLInputElement) {
+              onInterestRateChange(Number(e.target.value));
+            }
+          }}
+          disabled={inputsDisabled}
+          className="w-full mb-2 border border-gray-500 rounded-md"
+        />
       </div>
 
       <div className="w-full">
-      <label>Loan Term:</label>
-      <input
-        type="number"
-        value={loanTerm}
-        onChange={(e) => {
-          if (e.target instanceof HTMLInputElement) {
-            onLoanTermChange(Number(e.target.value));
-          }
-        }}
-        disabled={inputsDisabled}
-        className="w-full mb-2 border border-gray-500 rounded-md"
-      />
+        <label>Loan Term:</label>
+        <input
+          type="number"
+          value={loanTerm}
+          onChange={(e) => {
+            if (e.target instanceof HTMLInputElement) {
+              onLoanTermChange(Number(e.target.value));
+            }
+          }}
+          disabled={inputsDisabled}
+          className="w-full mb-2 border border-gray-500 rounded-md"
+        />
       </div>
 
       <div className="w-4/5 mb-3">
@@ -166,38 +166,38 @@ const MortgageForm: React.FC<MortgageFormProps> = ({
       </div>
 
       <div className="w-full flex flex-col mb-3">
-      <div className="flex">
-        <div className="w-1/2 mr-2">
-          <label>Province:</label>
-          <select
-            value={selectedProvince ?? ""}
-            onChange={handleProvinceChange}
-            disabled={inputsDisabled}
-            className="w-full border rounded-md font-normal"
-          >
-            <option value="">Select Province</option>
-            {provinces.map((province) => (
-              <option key={province.province_id} value={province.province_id}>
-                {province.description}
-              </option>
-            ))}
-          </select>
+        <div className="flex">
+          <div className="w-1/2 mr-2">
+            <label>Province:</label>
+            <select
+              value={selectedProvince ?? ""}
+              onChange={handleProvinceChange}
+              disabled={inputsDisabled}
+              className="w-full border rounded-md font-normal"
+            >
+              <option value="">Select Province</option>
+              {provinces.map((province) => (
+                <option key={province.province_id} value={province.province_id}>
+                  {province.description}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="w-1/2 ml-2">
+            <label>City:</label>
+            <select
+              disabled={!selectedProvince || inputsDisabled}
+              className="w-full border rounded-md font-normal"
+            >
+              <option value="">Select City</option>
+              {cities.map((city) => (
+                <option key={city.city_id} value={city.city_id}>
+                  {city.description}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-        <div className="w-1/2 ml-2">
-          <label>City:</label>
-          <select
-            disabled={!selectedProvince || inputsDisabled}
-            className="w-full border rounded-md font-normal"
-          >
-            <option value="">Select City</option>
-            {cities.map((city) => (
-              <option key={city.city_id} value={city.city_id}>
-                {city.description}
-              </option>
-            ))}
-          </select>
-        </div>
-      </div>
       </div>
 
       {showExtraFields && (
