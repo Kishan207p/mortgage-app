c50268b75be90e7c007f50b70b118b463fec689a
Kishan Patel <kishan207.p@gmail.com>
2024-04-05 18:27:25 -0400

    progress

--- Frontend/src/components/step.tsx
+++ Frontend/src/components/step.tsx
@@ -1,4 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoCreateOutline, IoDocumentsOutline } from "react-icons/io5";
+import { FaCheck } from "react-icons/fa";
+import { MdCreditScore } from "react-icons/md";
+import { BsUpload, BsPen } from "react-icons/bs";
+import { BiFolder } from "react-icons/bi";
+import { GiHouseKeys } from "react-icons/gi";
+import { GoIssueClosed } from "react-icons/go";
+import { BiHome } from "react-icons/bi";
 
 interface StepProps {
   currentStep: number;
@@ -7,33 +15,106 @@ interface StepProps {
   handlePrevious: () => void;
 }
 
-const Steps: React.FC<StepProps> = ({ currentStep, steps, handleNext, handlePrevious }) => {
+const ioCreateArray: JSX.Element[] = [
+  <IoCreateOutline />,
+  <MdCreditScore />,
+  <BsUpload />,
+  <BiFolder />,
+  <BsPen />,
+  <IoDocumentsOutline />,
+  <GiHouseKeys />,
+  <GoIssueClosed />,
+  <BiHome />,
+  <IoCreateOutline />,
+];
+
+const Steps: React.FC<StepProps> = ({
+  currentStep,
+  steps,
+  handleNext,
+  handlePrevious,
+}) => {
+  const [completedSteps, setCompletedSteps] = useState<number[]>([]);
+
+  const toggleCompletion = (index: number) => {
+    if (completedSteps.includes(index)) {
+      setCompletedSteps(completedSteps.filter((step) => step !== index));
+    } else {
+      setCompletedSteps([...completedSteps, index]);
+    }
+  };
+
+  const handlePreviousStep = () => {
+    // If the current step was marked as completed, unmark it
+    if (completedSteps.includes(currentStep)) {
+      setCompletedSteps(completedSteps.filter((step) => step !== currentStep));
+    }
+    handlePrevious();
+  };
+
   return (
     <div className="flex flex-col items-center">
-      <div className="flex items-center justify-between w-full">
-        {steps.map((step, index) => (
+      <div className="flex items-center justify-between w-4/5">
+        {steps.map((_step, index) => (
           <React.Fragment key={index}>
-            <div className={`w-12 h-12 flex items-center justify-center rounded-full border-2 ${index <= currentStep ? 'bg-blue-500 border-blue-500' : 'border-gray-300'}`}>
-              <span className={`text-lg ${index <= currentStep ? 'text-white' : 'text-gray-500'}`}>{index + 1}</span>
+            <div
+              className={`w-7/12 h-12 flex items-center justify-center rounded-full border-2 ${
+                index <= currentStep
+                  ? "bg-blue-500 border-blue-500"
+                  : "border-gray-300"
+              }`}
+              onClick={() => toggleCompletion(index)}
+            >
+              <span
+                className={`text-lg ${
+                  index <= currentStep ? "text-white" : "text-gray-500"
+                }`}
+              >
+                {completedSteps.includes(index) ? (
+                  <FaCheck />
+                ) : (
+                  ioCreateArray[index]
+                )}
+              </span>
             </div>
             {index !== steps.length - 1 && (
-              <div className="h-1 w-full bg-gray-300"></div>
+              <div
+                className={`h-1 w-full ${
+                  index + 1 <= currentStep ? "bg-blue-500" : "bg-gray-300"
+                }`}
+              ></div>
             )}
           </React.Fragment>
         ))}
       </div>
-      <div className="flex items-center justify-between w-full">
+      <div className="flex items-center justify-between w-4/5">
         {steps.map((step, index) => (
-          <span key={index} className={`text-xs ${index === currentStep ? 'text-blue-500' : 'text-gray-500'}`}>
-            {index === currentStep ? step.name : ''}
+          <span
+            key={index}
+            className={`text-center ${
+              index === currentStep ? "text-blue-500" : "text-gray-500"
+            }`}
+          >
+            {index === currentStep ? step.name : ""}
           </span>
         ))}
       </div>
       <div className="mt-4">
-        <button className="px-4 py-2 bg-blue-500 text-white rounded" onClick={handlePrevious} disabled={currentStep === 0}>
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded"
+          onClick={handlePreviousStep}
+          disabled={currentStep === 0}
+        >
           Previous
         </button>
-        <button className="px-4 py-2 bg-blue-500 text-white rounded" onClick={handleNext} disabled={currentStep === steps.length - 1}>
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded"
+          onClick={() => {
+            toggleCompletion(currentStep);
+            handleNext();
+          }}
+          disabled={currentStep === steps.length - 1}
+        >
           Next
         </button>
       </div>
--- Frontend/src/pages/DocumentPage.tsx
+++ Frontend/src/pages/DocumentPage.tsx
@@ -1,17 +1,18 @@
 // DocumentPage.tsx
-import React, { useState } from 'react';
-import Steps from '../components/step';
-import CreateApplication from '../components/phases/CreateApplication';
-import CreditCheck from '../components/phases/CreditCheck';
-import UploadDocuments from '../components/phases/UploadDocuments';
-import LoanProcessing from '../components/phases/LoanProcessing';
-import Underwriting from '../components/phases/Underwriting';
-import RiskAssessment from '../components/phases/RiskAssessment';
-import ConditionallyApproved from '../components/phases/ConditionallyApproved';
-import ClearToClose from '../components/phases/ClearToClose';
-import Closing from '../components/phases/Closing';
-import LoanFunded from '../components/phases/LoanFunded';
+import React, { useState } from "react";
+import Steps from "../components/step";
+import CreateApplication from "../components/phases/CreateApplication";
+import CreditCheck from "../components/phases/CreditCheck";
+import UploadDocuments from "../components/phases/UploadDocuments";
+import LoanProcessing from "../components/phases/LoanProcessing";
+import Underwriting from "../components/phases/Underwriting";
+import RiskAssessment from "../components/phases/RiskAssessment";
+import ConditionallyApproved from "../components/phases/ConditionallyApproved";
+import ClearToClose from "../components/phases/ClearToClose";
+import Closing from "../components/phases/Closing";
+import LoanFunded from "../components/phases/LoanFunded";
 import { IoCreateOutline } from "react-icons/io5";
+import Header from "../components/Header";
 
 const DocumentPage: React.FC = () => {
   //const navigate = useNavigate();
@@ -22,10 +23,14 @@ const DocumentPage: React.FC = () => {
     { path: "/loan-processing", name: "Loan Processing", completed: false },
     { path: "/underwriting", name: "Underwriting", completed: false },
     { path: "/risk-assessment", name: "Risk Assessment", completed: false },
-    { path: "/conditionally-approved", name: "Conditionally Approved", completed: false },
+    {
+      path: "/conditionally-approved",
+      name: "Conditionally Approved",
+      completed: false,
+    },
     { path: "/clear-to-close", name: "Clear To Close", completed: false },
     { path: "/closing", name: "Closing", completed: false },
-    { path: "/loan-funded", name: "Loan Funded", completed: false }
+    { path: "/loan-funded", name: "Loan Funded", completed: false },
   ]);
 
   const [currentStep, setCurrentStep] = useState(0);
@@ -50,22 +55,25 @@ const DocumentPage: React.FC = () => {
 
   return (
     <>
-      <Steps 
-        currentStep={currentStep} 
-        steps={steps} 
-        handleNext={handleNextStep} 
-        handlePrevious={handlePreviousStep} 
-      />
-      {currentStep === 0 && <CreateApplication />}
-      {currentStep === 1 && <CreditCheck />}
-      {currentStep === 2 && <UploadDocuments />}
-      {currentStep === 3 && <LoanProcessing />}
-      {currentStep === 4 && <Underwriting />}
-      {currentStep === 5 && <RiskAssessment />}
-      {currentStep === 6 && <ConditionallyApproved />}
-      {currentStep === 7 && <ClearToClose />}
-      {currentStep === 8 && <Closing />}
-      {currentStep === 9 && <LoanFunded />}
+      <Header />
+      <div className="container mx-auto px-2 py-8">
+        <Steps
+          currentStep={currentStep}
+          steps={steps}
+          handleNext={handleNextStep}
+          handlePrevious={handlePreviousStep}
+        />
+        {currentStep === 0 && <CreateApplication />}
+        {currentStep === 1 && <CreditCheck />}
+        {currentStep === 2 && <UploadDocuments />}
+        {currentStep === 3 && <LoanProcessing />}
+        {currentStep === 4 && <Underwriting />}
+        {currentStep === 5 && <RiskAssessment />}
+        {currentStep === 6 && <ConditionallyApproved />}
+        {currentStep === 7 && <ClearToClose />}
+        {currentStep === 8 && <Closing />}
+        {currentStep === 9 && <LoanFunded />}
+      </div>
     </>
   );
 };
